cc6acf3d8d667ddaf590304cea09e3193e6b7df0
MinhyeokSun <s.minyeok@gmail.com>
2021-12-16 23:49:16 +0900

    6th Commit

--- app/src/main/java/com/smh/photoframe/PhotoFrameActivity.kt
+++ app/src/main/java/com/smh/photoframe/PhotoFrameActivity.kt
@@ -4,6 +4,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 import kotlin.concurrent.timer
 
 class PhotoFrameActivity: AppCompatActivity() {  // 5초에 한번씩 이미지 바뀌게 보여주기.
@@ -11,6 +12,8 @@ class PhotoFrameActivity: AppCompatActivity() {  // 5초에 한번씩 이미지 
     private val photoList = mutableListOf<Uri>()
 
     private var currentPosition = 0
+
+    private var timer: Timer? = null
 
     private val backgroundPhotoImageView by lazy {
         findViewById<ImageView>(R.id.backgroundPhotoImageView)
@@ -29,6 +32,22 @@ class PhotoFrameActivity: AppCompatActivity() {  // 5초에 한번씩 이미지 
         startTimer()
     }
 
+    override fun onStop() {
+        super.onStop()
+        timer?.cancel()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        startTimer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        timer?.cancel()
+    }
+
     private fun getPhotoUriFromIntent() {
         val size = intent.getIntExtra("photoListSize", 0)
         for (i in 0..size) {
@@ -39,7 +58,7 @@ class PhotoFrameActivity: AppCompatActivity() {  // 5초에 한번씩 이미지 
     }
 
     private fun startTimer() {   // 5초에 한번씩 실행하기.
-        timer(period = 5000) {
+        timer = timer(period = 5000) {
             runOnUiThread {
                 val current = currentPosition
                 val next = if (photoList.size <= currentPosition + 1) 0 else currentPosition + 1
