e8d74beddb770e4a176af96601079782a72bb037
MinhyeokSun <s.minyeok@gmail.com>
2021-12-16 22:38:36 +0900

    4th Commit

--- app/src/main/java/com/smh/photoframe/MainActivity.kt
+++ app/src/main/java/com/smh/photoframe/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.smh.photoframe
 
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -30,6 +32,8 @@ class MainActivity : AppCompatActivity() {
             add(findViewById(R.id.imageView23))
         }
     }
+
+    private val imageUriList: MutableList<Uri> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -100,6 +104,35 @@ class MainActivity : AppCompatActivity() {
             .setNegativeButton("취소하기") { _, _ ->}
             .create()
             .show()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode != Activity.RESULT_OK) {
+            return
+        }
+
+        when(requestCode) {
+            2000 -> {
+                val selectedImageUri: Uri? = data?.data
+                if (selectedImageUri != null) {
+
+                    if(imageUriList.size == 6) {
+                        Toast.makeText(this, "이미 사진이 꽉 찼습니다.",Toast.LENGTH_SHORT).show()
+                        return
+                    }
+
+                    imageUriList.add(selectedImageUri)
+                    imageViewList[imageUriList.size - 1].setImageURI(selectedImageUri)
+                } else {
+                    Toast.makeText(this, "사진을 가져오지 못했습니다.",Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> {
+                Toast.makeText(this, "사진을 가져오지 못했습니다.",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun initStartPhotoFrameModeButton() {
